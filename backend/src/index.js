0803005b4a0bb3b48a1c25d19247fefb03675167
Danesh <techydanesh@gmail.com>
2025-09-24 18:00:50 +0530

    static file wilde route * to *name

--- backend/src/index.js
+++ backend/src/index.js
@@ -26,7 +26,7 @@ app.use(
 //routes
 app.use("/api", router);
 
-app.get("*", (req, res) => {
+app.get("*name", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "public", "index.html"));
 });
 //error handler
